5b9fea7c9a388303a99eeb932da75d4c02145728
UE-DND <100979820+UE-DND@users.noreply.github.com>
2026-09-26 15:46:47 +0800

    📱 禁用 Android WebView 默认触觉反馈

--- apps/mobile/android/app/src/main/java/org/uednd/chronos/MainActivity.java
+++ apps/mobile/android/app/src/main/java/org/uednd/chronos/MainActivity.java
@@ -1,5 +1,14 @@
 package org.uednd.chronos;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		if (bridge != null && bridge.getWebView() != null) {
+			bridge.getWebView().setHapticFeedbackEnabled(false);
+		}
+	}
+}
